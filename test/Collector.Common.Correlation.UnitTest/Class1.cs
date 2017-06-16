d06f4be431a0e096ed63372d5d490ee809e3d1a8
martin.bergkvist <Martin.bergqvist@collectorbank.se>
2017-06-16 09:19:52 +0200

    added unittests

---
+++ test/Collector.Common.Correlation.UnitTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace Collector.Common.Correlation.UnitTest
+{
+    using Xunit;
+    public class Class1
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
